252ee25e76f2d561a0f841ffb305e183a2fe605a
pavel <pavelkrikunenko@yandex.ru>
2023-08-25 19:05:02 +0300

    1

--- client.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-from pprint import pprint
-
-if __name__ == "__main__":
-    conn = sqlite3.connect("./companies.db")
-    for i in conn.cursor().execute("SELECT * FROM companies").fetchall():
-        pprint(i)
